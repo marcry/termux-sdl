5ad5c2821de026b98366d0626875fde7d04dff95
Lzhiyong <975434530@qq.com>
2021-03-18 22:11:51 +0800

    update

--- app/src/main/cpp/ffplay/ffplay.c
+++ app/src/main/cpp/ffplay/ffplay.c
@@ -1798,8 +1798,8 @@ static void toggle_mute(VideoState *is) {
 }
 
 static void update_volume(VideoState *is, int sign, double step) {
-    double volume = is->audio_volume ? (20 * log(is->audio_volume / (double)SDL_MIX_MAXVOLUME) / log(10)) : -1000.0;
-    int new_volume = lrint(SDL_MIX_MAXVOLUME * pow(10.0, (volume + sign * step) / 20.0));
+    double volume_level = is->audio_volume ? (20 * log(is->audio_volume / (double)SDL_MIX_MAXVOLUME) / log(10)) : -1000.0;
+    int new_volume = lrint(SDL_MIX_MAXVOLUME * pow(10.0, (volume_level + sign * step) / 20.0));
     is->audio_volume = av_clip(is->audio_volume == new_volume ? (is->audio_volume + sign) : new_volume, 0, SDL_MIX_MAXVOLUME);
 }
 
