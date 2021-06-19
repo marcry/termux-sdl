88ecec63508005007009b8c822d7cb15c75cc411
Lzhiyong <975434530@qq.com>
2021-06-19 11:02:50 +0800

    update

--- app/src/main/java/com/termux/sdl/TermuxNativeActivity.java
+++ app/src/main/java/com/termux/sdl/TermuxNativeActivity.java
@@ -28,8 +28,10 @@ public class TermuxNativeActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         nativeApp = getIntent().getStringExtra("nativeApp");
+        // copy libxxx.so to internal directory
+        copyLibFile();
+        
         Intent intent = new Intent(this, NativeActivity.class);
-        // from jni: native_loader.cpp
         intent.putExtra("nativeApp", nativeApp);
         // start native app
         startActivity(intent);
@@ -37,32 +39,34 @@ public class TermuxNativeActivity extends Activity {
     }
 
     public void copyLibFile() {
-        Path app = Paths.get(nativeApp);
-        if(Files.exists(app)) {
-            Path dir = Paths.get(getCacheDir().getParentFile().getAbsolutePath() + "/tmpdir");
-            Path file = dir.resolve(app.getFileName());
+        if(nativeApp != null && !nativeApp.isEmpty()) {
+            Path app = Paths.get(nativeApp);
+            if(Files.exists(app)) {
+                Path dir = Paths.get(getCacheDir().getParentFile().getAbsolutePath() + "/tmpdir");
+                Path file = dir.resolve(app.getFileName());
 
-            try {
-                if(!Files.exists(dir))
-                    Files.createDirectories(dir);
+                try {
+                    if(!Files.exists(dir))
+                        Files.createDirectories(dir);
 
-                Files.copy(app, file, StandardCopyOption.REPLACE_EXISTING);
-                Runtime.getRuntime().exec("chmod 755 " + file.toAbsolutePath()).waitFor();
+                    Files.copy(app, file, StandardCopyOption.REPLACE_EXISTING);
+                    Runtime.getRuntime().exec("chmod 755 " + file.toAbsolutePath()).waitFor();
 
-                // Environment variables must be set, otherwise the program will not run correctly
-                String pwd = app.getParent().toAbsolutePath().toString();
-                Log.i(TAG, "chdir: " + pwd);
-                JNI.chDir(pwd);
-                JNI.setEnv("PWD", pwd, true);
-                // nativeApp = /data/user/0/com.termux.sdl/tmpdir/libxxx.so
-                nativeApp = file.toAbsolutePath().toString();
+                    // Environment variables must be set, otherwise the program will not run correctly
+                    String pwd = app.getParent().toAbsolutePath().toString();
+                    Log.i(TAG, "chdir: " + pwd);
+                    JNI.chDir(pwd);
+                    JNI.setEnv("PWD", pwd, true);
+                    // nativeApp = /data/user/0/com.termux.sdl/tmpdir/libxxx.so
+                    nativeApp = file.toAbsolutePath().toString();
 
-                FileOutputStream conf = new FileOutputStream(dir + "/native_loader.conf");
-                conf.write(nativeApp.getBytes());
-                conf.close();
+                    FileOutputStream conf = new FileOutputStream(dir + "/native_loader.conf");
+                    conf.write(nativeApp.getBytes());
+                    conf.close();
 
-            } catch(Exception e) {
-                e.printStackTrace();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -88,3 +92,4 @@ public class TermuxNativeActivity extends Activity {
         deleteLibFile();
     }
 }
+
